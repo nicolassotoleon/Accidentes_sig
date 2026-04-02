8c72e591c5d5027cb3f09511e289de2f6e4aada8
nicolassotoleon <61476330+nicolassotoleon@users.noreply.github.com>
2026-04-02 10:14:20 -0500

    Update conexion.php
    v2

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@ function conexion(){
 $host = 'localhost';
 $port = '5432';
 $base_datos = 'incidentes_cali';
-$usuario = 'postgre';
+$usuario = 'postgres';
 $pass = 'admin';
 $conexion = pg_connect("host=$host port=$port dbname=$base_datos user=$usuario password=$pass")
             or die("Error de Conexion".pg_last_error());
